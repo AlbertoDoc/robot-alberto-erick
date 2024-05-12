63c38469acf86af1f6fe2d3129a6bcdbccb15971
Alberto Oliveira Santos <albertooliveirasantos@gmail.com>
2024-05-12 01:43:41 -0300

    Update AlbertoErickRobot.java

--- AlbertoErickRobot.java
+++ AlbertoErickRobot.java
@@ -11,9 +11,9 @@ import org.jpl7.Term;
 // Feito por Alberto Oliveira Santos e Erick Silva Kokubum
 
 public class AlbertoErickRobot extends AdvancedRobot {
-    private static final int HIT_NUMBERS_TO_SWITCH_MOVEMENT = 3;
-    private static final long HIT_EVALUATE_INTERVAL = 3000; // 3 segundos em milissegundos
-    private static final long HIT_WALL_EVALUATE_INTERVAL = 6000; // 6 segundos em milissegundos
+    private static final int HIT_NUMBERS_TO_SWITCH_MOVEMENT = 5;
+    private static final long HIT_EVALUATE_INTERVAL = 8886; // 3 segundos em milissegundos
+    private static final long HIT_WALL_EVALUATE_INTERVAL = 4669; // 6 segundos em milissegundos
     int numberOfHits = 0;
     long firstHitTime = 0;
     private byte directionMovementSign = 1;
@@ -27,18 +27,18 @@ public class AlbertoErickRobot extends AdvancedRobot {
     double enemyLastAbsBearing = 0.0;
 
     public void run() {
-    	if (!Query.hasSolution("consult('script.pl').")){
-    		System.out.println("Consult failed");
-    	}
-    	
+        if (!Query.hasSolution("consult('script.pl').")){
+            System.out.println("Consult failed");
+        }
+
         setup();
 
         while(true) {
-        	Query evaluatHitIntervalQ = new Query("evaluateHitInterval",new Term[] {
-	           new org.jpl7.Integer(getTime()),
-	           new org.jpl7.Integer(firstHitTime),
-	           new org.jpl7.Integer(HIT_EVALUATE_INTERVAL)
-        	});
+            Query evaluatHitIntervalQ = new Query("evaluateHitInterval",new Term[] {
+                    new org.jpl7.Integer(getTime()),
+                    new org.jpl7.Integer(firstHitTime),
+                    new org.jpl7.Integer(HIT_EVALUATE_INTERVAL)
+            });
             if (evaluatHitIntervalQ.hasSolution()) {
                 System.out.println("Limite de tempo ultrapassado - zerando hits");
 
@@ -70,7 +70,7 @@ public class AlbertoErickRobot extends AdvancedRobot {
     public void onHitByBullet(HitByBulletEvent e) {
         numberOfHits++;
         Query oneHitQ = new Query("numberOfHitsIsOne",new Term[] {
-           new org.jpl7.Integer(numberOfHits)
+                new org.jpl7.Integer(numberOfHits)
         });
         if(oneHitQ.hasSolution()) {
             System.out.println("Primeiro HIT");
@@ -97,14 +97,14 @@ public class AlbertoErickRobot extends AdvancedRobot {
 
         hitWallCount++;
         Query oneHitQ = new Query("numberOfHitsIsOne",new Term[] {
-           new org.jpl7.Integer(hitWallCount)
+                new org.jpl7.Integer(hitWallCount)
         });
         if(oneHitQ.hasSolution()) {
             System.out.println("Primeiro HIT Wall");
 
             firstHitWallTime = getTime();
         }
-        
+
         Query hitWallCheckQ = new Query("hitWallCountAndTimeCheck",new Term[] {
                 new org.jpl7.Integer(hitWallCount),
                 new org.jpl7.Integer(getTime()),
@@ -112,7 +112,7 @@ public class AlbertoErickRobot extends AdvancedRobot {
                 new org.jpl7.Integer(HIT_NUMBERS_TO_SWITCH_MOVEMENT),
                 new org.jpl7.Integer(HIT_WALL_EVALUATE_INTERVAL)
         });
-        
+
 
 
         if (hitWallCheckQ.hasSolution()) {
@@ -146,10 +146,10 @@ public class AlbertoErickRobot extends AdvancedRobot {
     }
 
     public void doMoveInCircle() {
-    	Query zeroVelocityQ = new Query("velocityIsZero",new Term[] {
-    		new org.jpl7.Float(getVelocity())
-    	});
-        setMaxVelocity(rand.nextDouble() * 8 + 2); // Velocidade aleatória entre 2 e 10
+        Query zeroVelocityQ = new Query("velocityIsZero",new Term[] {
+                new org.jpl7.Float(getVelocity())
+        });
+        setMaxVelocity(4.258287428314286); // Velocidade gerada por algoritmo genetico
         if (zeroVelocityQ.hasSolution()) {
             directionMovementSign *= -1;
         }
@@ -161,15 +161,15 @@ public class AlbertoErickRobot extends AdvancedRobot {
 
 
     public void doMoveInZigZag() {
-    	
-        setMaxVelocity(rand.nextDouble() * 8 + 2); // Velocidade aleatória entre 2 e 10
+
+        setMaxVelocity(3.8539549527045995); // Velocidade gerada por algoritmo genetico
 
         setTurnRight(setNormalizeAngleForBearing(enemyBearing + 90));
         execute();
 
         System.out.println(getTime() % 20);
         Query mod20Q = new Query("timeModulo20",new Term[] {
-        		new org.jpl7.Integer(getTime())
+                new org.jpl7.Integer(getTime())
         });
 
         if (mod20Q.hasSolution()) {
@@ -182,11 +182,11 @@ public class AlbertoErickRobot extends AdvancedRobot {
     }
 
     double setNormalizeAngleForBearing(double angle) {
-    	Query greaterQ = new Query("angleGreaterThan180",new Term[] {
-        		new org.jpl7.Float(angle)
+        Query greaterQ = new Query("angleGreaterThan180",new Term[] {
+                new org.jpl7.Float(angle)
         });
-    	Query lessQ = new Query("angleLessThanMinus180",new Term[] {
-        		new org.jpl7.Float(angle)
+        Query lessQ = new Query("angleLessThanMinus180",new Term[] {
+                new org.jpl7.Float(angle)
         });
 
 
@@ -200,8 +200,8 @@ public class AlbertoErickRobot extends AdvancedRobot {
     }
 
     void doShootStrategy(ScannedRobotEvent e) {
-    	Query zeroVelocityQ = new Query("velocityIsZero",new Term[] {
-        		new org.jpl7.Float(e.getVelocity())
+        Query zeroVelocityQ = new Query("velocityIsZero",new Term[] {
+                new org.jpl7.Float(e.getVelocity())
         });
         System.out.println("Velocity: " + e.getVelocity());
         System.out.println("Heading: " + e.getHeading());
@@ -223,7 +223,7 @@ public class AlbertoErickRobot extends AdvancedRobot {
     }
 
     double calculateBulletPower(double distance) {
-        return Math.max(3.0 / (distance / 100.0), 0.5);
+        return Math.max(2.5724285554177673 / (distance / 41.21509269985089), 0.5);
     }
-    
+
 }
