63f5bd3ad463dedc1d878210b23a586c593bbebb
Alberto Oliveira Santos <albertooliveirasantos@gmail.com>
2024-07-28 21:29:45 -0300

    Update AlbertoErickRobot.java

--- AlbertoErickRobot.java
+++ AlbertoErickRobot.java
@@ -1,67 +1,127 @@
-package alberto;
+package albertoErick;
 
 import robocode.*;
 
 import java.util.*;
 import java.awt.*;
-import java.util.List;
+
+import org.eclipse.recommenders.jayes.BayesNet;
+import org.eclipse.recommenders.jayes.BayesNode;
+import org.eclipse.recommenders.jayes.inference.IBayesInferer;
+import org.eclipse.recommenders.jayes.inference.junctionTree.JunctionTreeAlgorithm;
+import org.jpl7.Query;
+import org.jpl7.Term;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
 
 // Feito por Alberto Oliveira Santos e Erick Silva Kokubum
 
 public class AlbertoErickRobot extends AdvancedRobot {
+	private BayesNet net;
+    private IBayesInferer inferrer;
+    private double enemyDistance;
+
+    
     private static final int HIT_NUMBERS_TO_SWITCH_MOVEMENT = 3;
     private static final long HIT_EVALUATE_INTERVAL = 3000; // 3 segundos em milissegundos
-    private static final long HIT_WALL_EVALUATE_INTERVAL = 6000; // 3 segundos em milissegundos
+    private static final long HIT_WALL_EVALUATE_INTERVAL = 6000; // 6 segundos em milissegundos
+    private static final double ENEMY_THRESHOLD_DISTANCE = 70.0; // distancia limit bayes do inimigo
+    private static final long HIT_TOO_MUCH_EVALUATE_INTERVAL = 5000; // 5 segundos em milissegundos
+    private static final int V_LOWER_LIMIT_SLOW = 2; 
+    private static final int V_LOWER_LIMIT_MEDIUM = 4; 
+    private static final int V_LOWER_LIMIT_FAST = 6; 
+
     int numberOfHits = 0;
     long firstHitTime = 0;
+    long hitTooMuchTime = 0;
+
     private byte directionMovementSign = 1;
     double enemyBearing = 0;
     int hitWallCount = 0;
     long firstHitWallTime = 0;
     Random rand = new Random();
     MoveStrategy moveStrategy = MoveStrategy.ZIG_ZAG;
-    List<MoveStrategy> moveStrategyList = Arrays.asList(MoveStrategy.ZIG_ZAG, MoveStrategy.CIRCLE);
-    int strategyIndex = 0;
     double enemyLastAbsBearing = 0.0;
+    boolean hitTooMuch = false;
+    
+    final static String fclFilePath = "./robots/albertoErick/albertoErickFcl.fcl";
+    private FunctionBlock firePower;
+    private FunctionBlock prediction;
+    private FIS fis = null;
 
     public void run() {
+    	if (!Query.hasSolution("consult('script.pl').")){
+    		System.out.println("Consult failed");
+    	}
+    	
+    	// Initialize fuzzy
+    	fis = FIS.load(fclFilePath, true);
+        firePower = fis.getFunctionBlock("firePower");
+        prediction = fis.getFunctionBlock("prediction");
+    	
         setup();
+     // Initialize the Bayesian network
+        initializeBayesianNetwork();
 
         while(true) {
-
-            if (getTime() - firstHitTime > HIT_EVALUATE_INTERVAL) {
+        	Query evaluatHitIntervalQ = new Query("evaluateHitInterval",new Term[] {
+	           new org.jpl7.Integer(System.currentTimeMillis()),
+	           new org.jpl7.Integer(firstHitTime),
+	           new org.jpl7.Integer(HIT_EVALUATE_INTERVAL)
+        	});
+            if (evaluatHitIntervalQ.hasSolution()) {
                 System.out.println("Limite de tempo ultrapassado - zerando hits");
 
                 numberOfHits = 0;
             }
+            System.out.println("move strategy");
+
             doMoveStrategy();
 
             turnRadarRightRadians(Double.POSITIVE_INFINITY);
         }
     }
+    
+    private void initializeBayesianNetwork() {
+    	net = new BayesNet();
+        System.out.println("Criando node MS -> " + MoveStrategy.ZIG_ZAG.toString() + " | " + MoveStrategy.CIRCLE.toString());
+        
+    	BayesNode enemyDistance = net.createNode("ED");
+    	enemyDistance.addOutcomes("CLOSE", "FAR");
+    	enemyDistance.setProbabilities(0.3, 0.7);
+    	
+    	BayesNode wallDistance = net.createNode("WD");
+    	wallDistance.addOutcomes("CLOSE", "FAR");
+    	wallDistance.setProbabilities(0.4, 0.6);
+
+    	BayesNode movementStrategy = net.createNode("MS");
+    	movementStrategy.addOutcomes(MoveStrategy.ZIG_ZAG.toString(), MoveStrategy.CIRCLE.toString());
+    	movementStrategy.setParents(Arrays.asList(enemyDistance, wallDistance));
+    	movementStrategy.setProbabilities(
+    			// ED - CLOSE
+    			0.2, 0.8,	// WD - CLOSE
+    			0.4, 0.6, 	// WD - FAR
+    			// ED - FAR
+    			0.1, 0.9,	// WD - CLOSE
+    			0.5, 0.5 	// WD - FAR
+    	);
+    	
+    	BayesNode velocityLowerLimit = net.createNode("V");
+    	velocityLowerLimit.addOutcomes("SLOW", "NORMAL", "FAST");
+    	velocityLowerLimit.setParents(Arrays.asList(enemyDistance));
+    	velocityLowerLimit.setProbabilities(
+    			0.1, 0.1, 0.8,		// ED - CLOSE
+    			0.25, 0.5, 0.25 	// ED - FAR
+    	);
+ 
+
+        inferrer = new JunctionTreeAlgorithm();
+        inferrer.setNetwork(net);
+    }
 
     private void setup() {
         setColors(Color.red, Color.blue, Color.white);
-    }
-
-    private WallNear isNearWall() {
-        if (getX() < 80.0) {
-            return WallNear.LEFT_WALL;
-        }
-
-        if (getX() > 740.0) {
-            return WallNear.RIGHT_WALL;
-        }
-
-        if (getY() < 80.0) {
-            return WallNear.BOTTOM_WALL;
-        }
-
-        if (getY() > 540.0) {
-            return WallNear.TOP_WALL;
-        }
-
-        return WallNear.NONE;
     }
 
     /**
@@ -71,6 +131,13 @@ public class AlbertoErickRobot extends AdvancedRobot {
         enemyBearing = e.getBearing();
         doMoveStrategy();
         doShootStrategy(e);
+        enemyDistance = e.getDistance();
+
+        if ((System.currentTimeMillis() - hitTooMuchTime) > HIT_TOO_MUCH_EVALUATE_INTERVAL) {
+            System.out.println("Resetando hit too much");
+
+            hitTooMuch = false;
+        }
     }
 
     /**
@@ -78,21 +145,30 @@ public class AlbertoErickRobot extends AdvancedRobot {
      */
     public void onHitByBullet(HitByBulletEvent e) {
         numberOfHits++;
-        if(numberOfHits == 1) {
+        Query oneHitQ = new Query("numberOfHitsIsOne",new Term[] {
+           new org.jpl7.Integer(numberOfHits)
+        });
+        if(oneHitQ.hasSolution()) {
             System.out.println("Primeiro HIT");
 
-            firstHitTime = getTime();
+            firstHitTime = System.currentTimeMillis();
         }
 
-        if(numberOfHits >= HIT_NUMBERS_TO_SWITCH_MOVEMENT && (getTime() - firstHitTime <= HIT_EVALUATE_INTERVAL)){
+        if(numberOfHits >= HIT_NUMBERS_TO_SWITCH_MOVEMENT && (System.currentTimeMillis() - firstHitTime <= HIT_EVALUATE_INTERVAL)){
             System.out.println("Mudando estratégia");
+            hitTooMuch = true;
+            hitTooMuchTime = System.currentTimeMillis();
+            
+            if(moveStrategy == MoveStrategy.ZIG_ZAG) {
+            	moveStrategy = MoveStrategy.CIRCLE;
+            }else {
+            	moveStrategy = MoveStrategy.ZIG_ZAG;
 
-            strategyIndex++;
-            int expectedIndex = strategyIndex % moveStrategyList.size();
-            moveStrategy = moveStrategyList.get(expectedIndex);
+            }
             numberOfHits = 0;
         }
-
+        
+  
     }
 
     /**
@@ -102,13 +178,28 @@ public class AlbertoErickRobot extends AdvancedRobot {
         System.out.println("HIT Wall");
 
         hitWallCount++;
-        if(numberOfHits == 1) {
+        Query oneHitQ = new Query("numberOfHitsIsOne",new Term[] {
+           new org.jpl7.Integer(hitWallCount)
+        });
+        if(oneHitQ.hasSolution()) {
             System.out.println("Primeiro HIT Wall");
 
             firstHitWallTime = getTime();
         }
-        if (hitWallCount >= HIT_NUMBERS_TO_SWITCH_MOVEMENT && (getTime() - firstHitWallTime <= HIT_WALL_EVALUATE_INTERVAL)) {
+        
+        Query hitWallCheckQ = new Query("hitWallCountAndTimeCheck",new Term[] {
+                new org.jpl7.Integer(hitWallCount),
+                new org.jpl7.Integer(getTime()),
+                new org.jpl7.Integer(firstHitWallTime),
+                new org.jpl7.Integer(HIT_NUMBERS_TO_SWITCH_MOVEMENT),
+                new org.jpl7.Integer(HIT_WALL_EVALUATE_INTERVAL)
+        });
+        
+
+        if (hitWallCheckQ.hasSolution()) {
+            System.out.println("Too much wall hit, changing to circle");
             hitWallCount = 0;
+            hitTooMuch = true;
             moveStrategy = MoveStrategy.CIRCLE;
         }
     }
@@ -120,25 +211,87 @@ public class AlbertoErickRobot extends AdvancedRobot {
     enum MoveStrategy {
         ZIG_ZAG, CIRCLE
     }
+    
+    private WallNear isNearWall() {
+        if (getX() < 60.0) {
+            return WallNear.LEFT_WALL;
+        }
+
+        if (getX() > 760.0) {
+            return WallNear.RIGHT_WALL;
+        }
+
+        if (getY() < 60.0) {
+            return WallNear.BOTTOM_WALL;
+        }
+
+        if (getY() > 560.0) {
+            return WallNear.TOP_WALL;
+        }
+
+        return WallNear.NONE;
+    }
 
     public void doMoveStrategy() {
+    	// Set evidence for Bayesian network
+    	Map<BayesNode,String> evidence = new HashMap<BayesNode,String>();
+    	System.out.println("proximo a parede: "+ isNearWall().toString());
+        evidence.put(net.getNode("WD"), isNearWall() == WallNear.NONE ? "FAR" : "CLOSE");
+        evidence.put(net.getNode("ED"), enemyDistance > ENEMY_THRESHOLD_DISTANCE ? "FAR" : "CLOSE");
+        
+        inferrer.setEvidence(evidence);
+
+        double[] beliefsMS = inferrer.getBeliefs(net.getNode("MS")); 
+        
+        if (!hitTooMuch) {
+        	if (beliefsMS[0] >= beliefsMS[1]) {
+            	moveStrategy = MoveStrategy.ZIG_ZAG;
+            }else if (beliefsMS[1] > beliefsMS[0]) {
+            	moveStrategy = MoveStrategy.CIRCLE;
+            }
+        }
+        
+        double[] beliefsV = inferrer.getBeliefs(net.getNode("V")); 
+        
+        int vLowerLimit = getVLowerLimit(beliefsV[0],beliefsV[1],beliefsV[2]);
+        System.out.println(vLowerLimit + " LOWER LIMIT V");
+
         switch (moveStrategy) {
             case ZIG_ZAG:
                 System.out.println("Começando ZIG ZAG");
 
-                doMoveInZigZag();
+                doMoveInZigZag(vLowerLimit);
                 break;
             case CIRCLE:
                 System.out.println("Começando CIRCLE");
 
-                doMoveInCircle();
+                doMoveInCircle(vLowerLimit);
                 break;
         }
     }
+    
+    public int getVLowerLimit(double slow, double medium, double fast) {
+    	if(slow >= medium && slow >= fast) {
+    		return V_LOWER_LIMIT_SLOW;
+    	}
+    	
+    	if(medium >= slow && medium >= fast) {
+    		return V_LOWER_LIMIT_MEDIUM;
+    	}
+    	
+    	if(fast >= slow && fast >= medium) {
+    		return V_LOWER_LIMIT_FAST;
+    	}
+    	
+    	return V_LOWER_LIMIT_SLOW;
+    }
 
-    public void doMoveInCircle() {
-        setMaxVelocity(rand.nextDouble() * 8 + 2); // Velocidade aleatória entre 2 e 10
-        if (isNearWall() != WallNear.NONE || getVelocity() == 0) {
+    public void doMoveInCircle(int vLowerLimit) {
+    	Query zeroVelocityQ = new Query("velocityIsZero",new Term[] {
+    		new org.jpl7.Float(getVelocity())
+    	});
+        setMaxVelocity(rand.nextDouble() * 8 + vLowerLimit); // Velocidade aleatória entre lower limit e 10 + lower limit
+        if (zeroVelocityQ.hasSolution()) {
             directionMovementSign *= -1;
         }
         setTurnRight(setNormalizeAngleForBearing(enemyBearing + 90));
@@ -146,17 +299,23 @@ public class AlbertoErickRobot extends AdvancedRobot {
 
         execute();
     }
+    
 
 
-    public void doMoveInZigZag() {
-        setMaxVelocity(rand.nextDouble() * 8 + 2); // Velocidade aleatória entre 2 e 10
+
+    public void doMoveInZigZag(int vLowerLimit) {
+    	
+        setMaxVelocity(rand.nextDouble() * 10 + vLowerLimit); // Velocidade aleatória entre lower limit e 10 + lower limit
 
         setTurnRight(setNormalizeAngleForBearing(enemyBearing + 90));
         execute();
 
         System.out.println(getTime() % 20);
+        Query mod20Q = new Query("timeModulo20",new Term[] {
+        		new org.jpl7.Integer(getTime())
+        });
 
-        if (getTime() % 20 == 0) {
+        if (mod20Q.hasSolution()) {
             directionMovementSign *= -1;
             setAhead(150 * directionMovementSign);
             execute();
@@ -166,37 +325,72 @@ public class AlbertoErickRobot extends AdvancedRobot {
     }
 
     double setNormalizeAngleForBearing(double angle) {
+    	Query greaterQ = new Query("angleGreaterThan180",new Term[] {
+        		new org.jpl7.Float(angle)
+        });
+    	Query lessQ = new Query("angleLessThanMinus180",new Term[] {
+        		new org.jpl7.Float(angle)
+        });
 
-        while (angle >  180) {
+
+        while (greaterQ.hasSolution()) {
             angle -= 360;
         }
-        while (angle < -180) {
+        while (lessQ.hasSolution()) {
             angle += 360;
         }
         return angle;
     }
 
     void doShootStrategy(ScannedRobotEvent e) {
+        Query zeroVelocityQ = new Query("velocityIsZero",new Term[] {
+                new org.jpl7.Float(e.getVelocity())
+        });
         System.out.println("Velocity: " + e.getVelocity());
         System.out.println("Heading: " + e.getHeading());
 
         double absoluteBearing = getHeadingRadians() + e.getBearingRadians();
-        if (e.getVelocity() == 0.0) {
+        double futureBearing = Math.abs(absoluteBearing - enemyLastAbsBearing);
+        if (zeroVelocityQ.hasSolution()) {
             setTurnGunRightRadians(
                     robocode.util.Utils.normalRelativeAngle(absoluteBearing -
                             getGunHeadingRadians()));
         } else {
-            double futureBearing = Math.abs(absoluteBearing - enemyLastAbsBearing);
+        	prediction.setVariable("enemySpeed", e.getVelocity());
+        	prediction.setVariable("distance", e.getDistance());
+        	prediction.setVariable("aimCorrection", futureBearing);
+        	
+        	prediction.evaluate();
+        	
+        	double result = prediction.getVariable("prediction").getValue();
+        	System.out.println("predictionResult: " + result);
             setTurnGunRightRadians(
-                    robocode.util.Utils.normalRelativeAngle((absoluteBearing + futureBearing) -
+                    robocode.util.Utils.normalRelativeAngle((absoluteBearing + result) -
                             getGunHeadingRadians()));
         }
+        
+        System.out.println("aimCorrection: " + futureBearing);
 
-        fire(calculateBulletPower(e.getDistance()));
+        fire(calculateBulletPower(getEnergy(), e.getDistance(), futureBearing, e.getVelocity()));
         enemyLastAbsBearing = absoluteBearing;
     }
 
-    double calculateBulletPower(double distance) {
-        return Math.max(3.0 / (distance / 100.0), 0.5);
-    }
+    double calculateBulletPower(double ourEnergy, double distance, double prediction, double enemySpeed) {
+    	firePower.setVariable("ourEnergy", ourEnergy);
+    	firePower.setVariable("distance", distance);
+    	firePower.setVariable("prediction", prediction);
+    	firePower.setVariable("enemySpeed", Math.abs(enemySpeed));
+    	
+    	System.out.println("ourEnergy" + ourEnergy);
+    	System.out.println("distance" + distance);
+    	System.out.println("prediction" + prediction);
+    	System.out.println("enemySpeed" + Math.abs(enemySpeed));
+    	
+    	firePower.evaluate();
+    	
+    	double result = firePower.getVariable("firePower").getValue();
+    	System.out.println("poder tiro: " + result);
+    	
+        return result;
+    }  
 }
